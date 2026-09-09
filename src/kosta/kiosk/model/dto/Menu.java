26693b7f12ff9da76de48d8d7b9f3d70fe94150d
Woo Park <Wpark0429@gmail.com>
2026-09-09 17:26:21 +0900

    [feat] MenuDTO 수정

--- src/kosta/kiosk/model/dto/Menu.java
+++ src/kosta/kiosk/model/dto/Menu.java
@@ -1,86 +1,118 @@
 package kosta.kiosk.model.dto;
 
+import java.time.LocalDateTime;
+
 public class Menu {
-	public enum HotIce {
-		HOT, ICE
-	}
-	
-	private int menuId;
-	private int categoryId;
-	private String menuName;
-	private String description;
-	private int price;
-	private HotIce hotIce;
-	
-	public Menu() {
-		
-	}
 
-	public Menu(int menuId, int categoryId, String menuName, String description, int price, HotIce hotIce) {
-		super();
-		this.menuId = menuId;
-		this.categoryId = categoryId;
-		this.menuName = menuName;
-		this.description = description;
-		this.price = price;
-		this.hotIce = hotIce;
-	}
+    public enum HotIce {
+        HOT, ICE
+    }
 
-	public int getMenuId() {
-		return menuId;
-	}
+    private int menuId;
+    private int categoryId;
+    private String menuName;
+    private String description;
+    private int price;
+    private HotIce hotIce;
+    private LocalDateTime createdAt;
+    private boolean soldout;
 
-	public void setMenuId(int menuId) {
-		this.menuId = menuId;
-	}
-	
-	public int getCategoryId() {
-		return categoryId;
-	}
-	
-	public void setCategoryId(int categoryId) {
-		this.categoryId = categoryId;
-	}
+    public Menu() {
+    }
 
-	public String getMenuName() {
-		return menuName;
-	}
+    // 기존 생성자
+    public Menu(int menuId, int categoryId, String menuName,
+                String description, int price, HotIce hotIce) {
+        this.menuId = menuId;
+        this.categoryId = categoryId;
+        this.menuName = menuName;
+        this.description = description;
+        this.price = price;
+        this.hotIce = hotIce;
+    }
 
-	public void setMenuName(String menuName) {
-		this.menuName = menuName;
-	}
+    // 전체 필드를 받는 생성자
+    public Menu(int menuId, int categoryId, String menuName,
+                String description, int price, HotIce hotIce,
+                LocalDateTime createdAt, boolean soldout) {
+        this(menuId, categoryId, menuName, description, price, hotIce);
+        this.createdAt = createdAt;
+        this.soldout = soldout;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public int getMenuId() {
+        return menuId;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setMenuId(int menuId) {
+        this.menuId = menuId;
+    }
 
-	public int getPrice() {
-		return price;
-	}
+    public int getCategoryId() {
+        return categoryId;
+    }
 
-	public void setPrice(int price) {
-		this.price = price;
-	}
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
 
-	public HotIce getHotIce() {
-		return hotIce;
-	}
+    public String getMenuName() {
+        return menuName;
+    }
 
-	public void setHotIce(HotIce hotIce) {
-		this.hotIce = hotIce;
-	}
+    public void setMenuName(String menuName) {
+        this.menuName = menuName;
+    }
 
-	@Override
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public HotIce getHotIce() {
+        return hotIce;
+    }
+
+    public void setHotIce(HotIce hotIce) {
+        this.hotIce = hotIce;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public boolean isSoldout() {
+        return soldout;
+    }
+
+    public void setSoldout(boolean soldout) {
+        this.soldout = soldout;
+    }
+
+    @Override
     public String toString() {
-        return "MenuDTO [menuId=" + menuId
+        return "Menu [menuId=" + menuId
                 + ", categoryId=" + categoryId
                 + ", menuName=" + menuName
+                + ", description=" + description
                 + ", price=" + price
-                + ", hotIce=" + hotIce + "]";	
-	}
+                + ", hotIce=" + hotIce
+                + ", createdAt=" + createdAt
+                + ", soldout=" + soldout + "]";
+    }
 }
-
