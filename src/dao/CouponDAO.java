39a6dfc239547fae6a5cedd02cd18753614fc478
skmagicsk-sys <skmagicsk@naver.com>
2026-09-09 17:33:12 +0900

    메서드명 컨벤션 변경

--- src/dao/CouponDAO.java
+++ src/dao/CouponDAO.java
@@ -1,11 +1,12 @@
 package dao;
 
 import dto.CouponDTO;
+import kosta.kiosk.exception.DMLException;
 
 import java.util.List;
 
 public interface CouponDAO {
-    List<CouponDTO> couponSelectByUser(int userId) throws DMLException; //쿠폰조회
-    int couponInsert(CouponDTO couponDTO) throws DMLException; //쿠폰발급
-    boolean couponDelete(int couponId) throws DMLException; //쿠폰삭제
+    List<CouponDTO> selectCouponByUser(int userId) throws DMLException; //쿠폰조회
+    int insertCoupon(CouponDTO couponDTO) throws DMLException; //쿠폰발급
+    boolean deleteCoupon(int couponId) throws DMLException; //쿠폰삭제
 }
