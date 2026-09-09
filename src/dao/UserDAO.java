67f1bcb5175b44dd927776b799c156fe6d5a196d
skmagicsk-sys <skmagicsk@naver.com>
2026-09-09 17:02:32 +0900

    날짜 추가

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -1,4 +1,11 @@
 package dao;
 
+import dto.UserDTO;
+
 public interface UserDAO {
+    UserDTO userSelectByPhone(String phone) throws DMLException; //회원조회
+    int userInsert(UserDTO userDTO) throws DMLException; //회원추가
+    boolean userUpdateStamp(int userId, int stamp) throws DMLException; //스탬프갱신
+    int userSelectStamp(int userId) throws DMLException; //스탬프조회
+
 }
