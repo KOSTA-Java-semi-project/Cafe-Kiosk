51111526ae7b851d5a81875d9d8d1ebef3a3e7eb
ImHyungsuk <limhs8630@gmail.com>
2026-09-09 14:22:42 +0900

    [feat] create Enum (Size,IceLevel)

---
+++ src/kosta/kiosk/model/dto/IceLevel.java
@@ -0,0 +1,24 @@
+package kosta.kiosk.model.dto;
+
+public enum IceLevel {
+	FEW("FEW"), LOT("LOT"), DEFAULT("DEFAULT");
+
+	private String code;
+
+	IceLevel(String code) {
+		this.code = code;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public IceLevel fromCode(String code) {
+		for (IceLevel i : values()) {
+			if (i.code.equals(code))
+				return i;
+		}
+		throw new IllegalArgumentException("Wrong code:" + code);
+	}
+
+}
