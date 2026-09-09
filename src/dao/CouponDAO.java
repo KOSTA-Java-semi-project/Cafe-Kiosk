6b1aba81ef256d31ba29df1381dbfa6447865de1
skmagicsk-sys <skmagicsk@naver.com>
2026-09-09 17:05:34 +0900

    날짜 추가

--- src/dao/CouponDAO.java
+++ src/dao/CouponDAO.java
@@ -1,4 +1,11 @@
 package dao;
 
+import dto.CouponDTO;
+
+import java.util.List;
+
 public interface CouponDAO {
+    List<CouponDTO> couponSelectByUser(int userId) throws DMLException; //쿠폰조회
+    int couponInsert(CouponDTO couponDTO) throws DMLException; //쿠폰발급
+    boolean couponDelete(int couponId) throws DMLException; //쿠폰삭제
 }
