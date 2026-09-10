6c75c15157ad51cb1442c67379852b4817a6b239
Beomsoo <pbs3839@naver.com>
2026-09-10 11:27:10 +0900

    [feat] OrderDAOImpl 수정

--- src/kosta/kiosk/model/dao/OrderDAOImpl.java
+++ src/kosta/kiosk/model/dao/OrderDAOImpl.java
@@ -6,7 +6,10 @@ import kosta.kiosk.model.dto.OrderDetail;
 import kosta.kiosk.model.dto.Size;
 import kosta.kiosk.util.DbManager;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,21 +18,98 @@ public class OrderDAOImpl implements OrderDAO {
 
     @Override
     public List<Order> selectOrderByUserId(int userId) throws SQLException {
-        return List.of();
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        List<Order> list = new ArrayList<>();
+
+        String sql = "select * from `order` where user_id like ?";
+        try {
+            conn = DbManager.getConnection();
+            stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, userId);
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                list.add(new Order(rs.getInt("order_id"), rs.getInt("user_id"), rs.getInt("sum"), rs.getTimestamp("created_at").toLocalDateTime()));
+            }
+
+        } finally {
+            DbManager.close(conn, stmt, rs);
+        }
+        return list;
     }
 
     @Override
     public int insertOrder(Order order) throws SQLException {
-        return 0;
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        String sql = "insert into `order` (order_id, user_id, sum, created_at) values (?, ?, ?, now())";
+
+        try {
+            conn = DbManager.getConnection();
+            stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, order.getOrderId());
+            stmt.setInt(2, order.getUserId());
+            stmt.setInt(3, order.getSum());
+
+        } finally {
+            DbManager.close(conn, stmt,null);
+        }return stmt.executeUpdate();
     }
 
     @Override
     public Order selectOrderByOrderId(int orderId) throws SQLException {
-        return null;
-    }
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        Order order = null;
+
+        String sql = "select * from `order` where order_id = ? ";
+        try {
+            conn = DbManager.getConnection();
+            stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, orderId);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                order = new Order(rs.getInt("order_id"), rs.getInt("user_id"), rs.getInt("sum"), rs.getTimestamp("created_at").toLocalDateTime());
+                List<OrderDetail> orderLineList = this.selectOrderDetails(order.getOrderId());//메소드 호출
+                order.setOrderDetailList(orderLineList);
+
+            }
+
+            } finally{
+                DbManager.close(conn, stmt, rs);
+            }
+            return order;
+        }
 
     @Override
     public List<OrderDetail> selectOrderDetails(int orderId) throws SQLException {
-        return List.of();
+        Connection con=null;
+        PreparedStatement ps=null;
+        ResultSet rs=null;
+        List<OrderDetail> list = new ArrayList<>();
+        try {
+            con = DbManager.getConnection();
+            ps= con.prepareStatement("select * from order_detail where  order_id=?");
+            ps.setInt(1, orderId);
+            rs = ps.executeQuery();
+
+            while(rs.next()) {
+                OrderDetail orderDetail  = new OrderDetail(rs.getInt("detail_id"),
+                        rs.getInt("order_id"),rs.getInt("menu_id"),
+                        rs.getInt("amount"),Size.valueOf(rs.getString("size")),
+                        rs.getInt("shot"),
+                        IceLevel.valueOf(rs.getString("ice")),rs.getInt("syrup"));
+                list.add(orderDetail);
+            }
+        }finally {
+            DbManager.close(con, ps, rs);
+        }
+        return list;
+
     }
 }
