1b673b4856001c001226e37175e8cfef2ab1787e
Beomsoo <pbs3839@naver.com>
2026-09-10 17:00:00 +0900

    [feat] feature/createOrderService 생성

---
+++ src/kosta/kiosk/model/service/OrderService.java
@@ -0,0 +1,54 @@
+package kosta.kiosk.model.service;
+
+import kosta.kiosk.model.dao.MenuDAO;
+import kosta.kiosk.model.dao.MenuDAOImpl;
+import kosta.kiosk.model.dao.OrderDAO;
+import kosta.kiosk.model.dao.OrderDAOImpl;
+import kosta.kiosk.model.dto.Menu;
+import kosta.kiosk.model.dto.Order;
+import kosta.kiosk.model.dto.OrderDetail;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class OrderService {
+
+    private OrderDAO orderDAO = new OrderDAOImpl();
+    private MenuDAO menuDAO = new MenuDAOImpl();
+
+    public int insertOrder(int userId, List<OrderDetail> orderDetailList) throws SQLException {
+        if (orderDetailList == null || orderDetailList.isEmpty()) {
+            throw new SQLException("주문할 메뉴가 없습니다.");
+        }
+        int sum = calculateSum(orderDetailList);
+        Order order = new Order(userId, sum, orderDetailList);
+        return orderDAO.insertOrder(order);
+    }
+
+    private int calculateSum(List<OrderDetail> orderDetailList) throws SQLException {
+        Map<Integer, Menu> menuMap = new HashMap<>();
+        for (Menu menu : menuDAO.menuSelectAll()) {
+            menuMap.put(menu.getMenuId(), menu);
+        }
+
+        int sum = 0;
+        for (OrderDetail detail : orderDetailList) {
+            Menu menu = menuMap.get(detail.getMenuId());
+            if (menu == null) {
+                throw new SQLException("존재하지 않는 메뉴입니다: menuId=" + detail.getMenuId());
+            }
+            sum += menu.getPrice() * detail.getAmount();
+        }
+        return sum;
+    }
+
+    public List<Order> selectOrderByUserId(int userId) throws SQLException {
+        return orderDAO.selectOrderByUserId(userId);
+    }
+
+    public Order selectOrderByOrderId(int orderId) throws SQLException {
+        return orderDAO.selectOrderByOrderId(orderId);
+    }
+}
