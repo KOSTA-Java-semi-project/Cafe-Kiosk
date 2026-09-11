65b8868fcc09dbe554e866a9da242203d7fc22e0
Beomsoo <pbs3839@naver.com>
2026-09-11 15:38:01 +0900

    [refactor]

--- src/kosta/kiosk/model/service/OrderService.java
+++ src/kosta/kiosk/model/service/OrderService.java
@@ -1,14 +1,18 @@
 package kosta.kiosk.model.service;
 
+import kosta.kiosk.model.dao.CategoryDAO;
+import kosta.kiosk.model.dao.CategoryDAOImpl;
 import kosta.kiosk.model.dao.MenuDAO;
 import kosta.kiosk.model.dao.MenuDAOImpl;
 import kosta.kiosk.model.dao.OrderDAO;
 import kosta.kiosk.model.dao.OrderDAOImpl;
+import kosta.kiosk.model.dto.Category;
 import kosta.kiosk.model.dto.Menu;
 import kosta.kiosk.model.dto.Order;
 import kosta.kiosk.model.dto.OrderDetail;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,8 +21,23 @@ public class OrderService {
 
     private OrderDAO orderDAO = new OrderDAOImpl();
     private MenuDAO menuDAO = new MenuDAOImpl();
+    private CategoryDAO categoryDAO = new CategoryDAOImpl();
 
-    public int insertOrder(int userId, List<OrderDetail> orderDetailList) throws SQLException {
+    public List<Category> getCategoryList() throws SQLException {
+        return categoryDAO.selectAllCategory();
+    }
+
+    public List<Menu> getMenuListByCategoryId(int categoryId) throws SQLException {
+        List<Menu> result = new ArrayList<>();
+        for (Menu menu : menuDAO.selectAllMenu()) {
+            if (menu.getCategoryId() == categoryId) {
+                result.add(menu);
+            }
+        }
+        return result;
+    }
+
+    public int insertOrder(Integer userId, List<OrderDetail> orderDetailList) throws SQLException {
         if (orderDetailList == null || orderDetailList.isEmpty()) {
             throw new SQLException("주문할 메뉴가 없습니다.");
         }
@@ -39,10 +58,6 @@ public class OrderService {
             sum += price * detail.getAmount();
         }
         return sum;
-    }
-
-    public List<Order> selectOrderByUserId(int userId) throws SQLException {
-        return orderDAO.selectOrderByUserId(userId);
     }
 
     public Order selectOrderByOrderId(int orderId) throws SQLException {
