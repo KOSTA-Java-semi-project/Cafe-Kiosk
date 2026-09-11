cb3946487e39d3591ac0df8d60c5c074e7c24d1b
Gun Woo Park <117787684+Gupark116@users.noreply.github.com>
2026-09-11 15:39:16 +0900

    ManagerDTO 생성

--- src/kosta/kiosk/model/dto/ManagerDTO.java
+++ src/kosta/kiosk/model/dto/ManagerDTO.java
@@ -1,5 +1,67 @@
 package kosta.kiosk.model.dto;
 
 public class ManagerDTO {
+	
+	private int adminId;
+	private String name;
+	private String id;
+	private String password;
+	
+	public ManagerDTO() {
+		
+	}
 
+	public ManagerDTO(int adminId, String name, String id, String password) {
+		this.adminId = adminId;
+		this.name = name;
+		this.id = id;
+		this.password = password;
+	}
+	
+	public ManagerDTO(String id, String password) {
+		this.id = id;
+		this.password = password;
+	
+	}
+
+	public int getAdminId() {
+		return adminId;
+	}
+
+	public void setAdminId(int adminId) {
+		this.adminId = adminId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public String toString() {
+		return "ManagerDTO [adminId=" + adminId
+				+ ", name=" + name
+				+ ", id=" + id + "]";
+	}
+	
+	
 }
