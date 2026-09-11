963e842c68edde37a0eaea7a4f6a4fa27e995914
Beomsoo <pbs3839@naver.com>
2026-09-11 12:16:51 +0900

    [feat] CategoryDAOImpl 생성

--- src/kosta/kiosk/model/dao/CategoryDAO.java
+++ src/kosta/kiosk/model/dao/CategoryDAO.java
@@ -1,9 +1,11 @@
 package kosta.kiosk.model.dao;
 
 import java.sql.SQLException;
+import java.util.List;
+
+import kosta.kiosk.model.dto.Category;
 
 public interface CategoryDAO {
     List<Category> selectAllCategory() throws SQLException;
-    Category selectCategoryById(int categoryId) throws SQLException;
-
+    Category selectCategoryBycategoryId(int categoryId) throws SQLException;
 }
